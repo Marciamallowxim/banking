5b94a1c1b7fb3edfa65a8f99dbe704dd4cf908cc
Marciamallowxim <110351665+Marciamallowxim@users.noreply.github.com>
2024-10-12 07:40:46 +0100

    Update settings.py

--- zealous_bank/settings.py
+++ zealous_bank/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = 'verify_otp'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
